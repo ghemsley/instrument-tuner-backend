e4eee357bbceab88209b9cc2d03050d7bc058db7
ghemsley <ghemsley@protonmail.ch>
2021-05-29 05:33:02 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+tunings = Tuning.create([{ name: 'Standard', notes: 'E2, A2, D3, G3, B3, E4' },
+                         { name: 'Drop D', notes: 'D2, A2, D3, G3, B3, E4' },
+                         { name: 'Drop C', notes: 'C2, G2, C3, F3, A3, D4' }])
+guitar = Instrument.create({name: 'Guitar'})
+tunings.each do |tuning|
+  tuning.instrument = guitar
+  tuning.save
+end
