f245eb054b6a746fc64caf721522c6d38a9ce8b5
ghemsley <ghemsley@protonmail.ch>
2021-05-31 23:29:44 -0500

    Update tunings controller

--- app/controllers/tunings_controller.rb
+++ app/controllers/tunings_controller.rb
@@ -1,11 +1,20 @@
 class TuningsController < ApplicationController
+  include JSONAPI::Filtering
+
   def index
+    allowed = %i[instrument_id]
     tunings = Tuning.all
-    render json: TuningSerializer.new(tunings)
+    jsonapi_filter(tunings, allowed) do |filtered|
+      render jsonapi: filtered.result
+    end
   end
 
   def show
     tuning = Tuning.find(params[:id])
     render json: TuningSerializer.new(tuning).serializable_hash.to_json
+  end
+
+  def create
+    logger.info(params)
   end
 end
