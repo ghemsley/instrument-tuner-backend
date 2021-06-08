2446307b56a99d43693503a25929ebe851661a6b
ghemsley <ghemsley@protonmail.ch>
2021-06-08 08:49:06 -0500

    Add instrument attribute validations

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,4 +1,7 @@
 class Instrument < ApplicationRecord
   has_many :tunings
   has_one_attached :image
+
+  validates :name, presence: true, format: { with: /\A[[:alpha:]]+\Z/ }
+  validates_associated :tunings
 end
