f42ee218d4240a4ada10bd7fd74485218f784aba
ghemsley <ghemsley@protonmail.ch>
2021-06-16 14:59:32 -0500

    Add image fetch feature to instruments controller

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -17,7 +17,11 @@ class InstrumentsController < ApplicationController
       tuning = Tuning.create(name: tuning_name, notes: tuning_string)
       tuning
     end
-    instrument = Instrument.create(name: instrument_name, tunings: instrument_tunings)
+    result = Unsplash::Photo.search(instrument_name, per_page = 1)
+    image_link = result.first.urls.small
+    image_artist = result.first.user.name
+    image_artist_link = result.first.user.links.html
+    instrument = Instrument.create(name: instrument_name, tunings: instrument_tunings, image_link: image_link, image_artist: image_artist, image_artist_link: image_artist_link)
     render json: InstrumentSerializer.new(instrument, include: [:tunings]).serializable_hash.to_json
   end
 end
