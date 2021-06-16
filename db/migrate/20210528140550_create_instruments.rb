7cb12af71a568cc98976790685343e491570a76f
ghemsley <ghemsley@protonmail.ch>
2021-06-16 14:56:35 -0500

    Update db schema

--- db/migrate/20210528140550_create_instruments.rb
+++ db/migrate/20210528140550_create_instruments.rb
@@ -2,6 +2,9 @@ class CreateInstruments < ActiveRecord::Migration[6.1]
   def change
     create_table :instruments do |t|
       t.string :name
+      t.string :image_link
+      t.string :image_artist
+      t.string :image_artist_link
 
       t.timestamps
     end
