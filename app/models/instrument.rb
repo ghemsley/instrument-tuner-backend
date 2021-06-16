3b6aeb592e52574bfd6f37da9e9c97c51c14fc93
ghemsley <ghemsley@protonmail.ch>
2021-06-16 14:58:20 -0500

    Update name attribute validations

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,7 +1,6 @@
 class Instrument < ApplicationRecord
   has_many :tunings
-  has_one_attached :image
 
-  validates :name, presence: true, format: { with: /\A[[:alpha:]]+\Z/ }
+  validates :name, presence: true, format: { with: /\A[[:alpha:] ]+\Z/ }
   validates_associated :tunings
 end
