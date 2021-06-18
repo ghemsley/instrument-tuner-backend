4c02941f5b2e83eabfc5e8de2cd8b62f3ede16f7
ghemsley <ghemsley@protonmail.ch>
2021-06-18 15:12:27 -0500

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tunings
-  resources :instruments
+  resources :instruments, only: %i[index show create destroy]
+  resources :tunings, only: %i[index show create destroy]
 end
