a2c7c2296b35b8ba318f596d168ae2bdb82c2fc2
ghemsley <ghemsley@protonmail.ch>
2021-06-20 18:23:37 -0500

    Add non-blank validation checks to models

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,6 +1,6 @@
 class Instrument < ApplicationRecord
   has_many :tunings, dependent: :destroy
 
-  validates :name, presence: true, format: { with: /\A[[:alpha:]\d\-\/() ]+\Z/ }
+  validates :name, presence: true, allow_blank: false, format: { with: /\A[[:alpha:]\d\-\/() ]+\Z/ }
   validates_associated :tunings
 end
--- app/models/tuning.rb
+++ app/models/tuning.rb
@@ -1,8 +1,9 @@
 class Tuning < ApplicationRecord
   belongs_to :instrument
 
-  validates :name, presence: true, format: { with: /\A[[:alpha:]\d\-\/() ]+\Z/ }
-  validates :notes, presence: true, format: { with: /\A([A-G]#?[0-9](, )?)*([A-G]#?[0-9](?!, \Z)){1}\Z/ }
+  validates :name, presence: true, allow_blank: false, format: { with: /\A[[:alpha:]\d\-\/() ]+\Z/ }
+  validates :notes, presence: true, allow_blank: false,
+                    format: { with: /\A([A-G]#?[0-9](, )?)*([A-G]#?[0-9](?!, \Z)){1}\Z/ }
 
   def notes_array
     notes.split(', ')
